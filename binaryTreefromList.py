420bd3a31a1b120f2a0784bd40900849aada5111
toliahoang <tung.istu@gmail.com>
2020-04-15 17:23:31 +0700

    Create binaryTreefromList.py

---
+++ binaryTreefromList.py
@@ -0,0 +1,12 @@
+def build_bst(my_list):
+  if len(my_list) == 0:
+    return None
+
+  mid_idx = len(my_list) // 2
+  mid_val = my_list[mid_idx]
+
+  tree_node = {"data": mid_val}
+  tree_node["left_child"] = build_bst(my_list[ : mid_idx])
+  tree_node["right_child"] = build_bst(my_list[mid_idx + 1 : ])
+
+  return tree_node
